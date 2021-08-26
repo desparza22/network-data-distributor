4999c0375a48c166b1b7fd0c193e2f3330385bff
Diego Esparza <dae1@williams.edu>
2021-08-26 19:30:26 -0400

    descriptor and ip/port fields no longer mutually exclusive

--- functionality/socket_address.cpp
+++ functionality/socket_address.cpp
@@ -1,133 +1,170 @@
 #include "socket_address.hpp"
 
 SocketAddress::SocketAddress(std::string ip, std::string port) {
-  initialize_SocketAddress(ip, port);
+  initialize_SocketAddress(ip, port, NO_DESCRIPTOR);
 }
 
 SocketAddress::SocketAddress(int descriptor) {
-  initialize_SocketAddress(descriptor);
+  initialize_SocketAddress(NO_IP_PORT, NO_IP_PORT, descriptor);
+}
+
+SocketAddress::SocketAddress(std::string ip, std::string port, int descriptor) {
+  initialize_SocketAddress(ip, port, descriptor);
 }
 
 //represent_address should come from as_string
 SocketAddress::SocketAddress(std::string represent_address) {
-  char first_char = represent_address[0];
+  std::string ip;
+  std::string port;
+  int descriptor;
   
-  if(first_char == DESCRIPTOR_HEADER) {
-    int descriptor_start = 1;
-    std::string descriptor_str = represent_address.substr(descriptor_start);
-
-    int descriptor = std::stoi(descriptor_str);
-    initialize_SocketAddress(descriptor);
-    
-  } else {
-    std::string ip;
-    std::string port;
+  int seperator1_index = represent_address.find(FIELD_SEP);
+  int seperator2_index = represent_address.substr(seperator1_index + 1).find(FIELD_SEP) + seperator1_index + 1; 
   
-    int ip_end = represent_address.find(IP_PORT_SEP);
-    int port_start = ip_end + 1;
+  int ip_start = 0;
+  int ip_end = seperator1_index;
+  int port_start = seperator1_index + 1;
+  int port_end = seperator2_index;
+  int descriptor_start = seperator2_index + 1;
   
-    ip = represent_address.substr(0, ip_end);
-    port = represent_address.substr(port_start);
-    initialize_SocketAddress(ip, port);
-  }
+  ip = represent_address.substr(ip_start, ip_end - ip_start);
+  port = represent_address.substr(port_start, port_end - port_start);
+  descriptor = std::stoi(represent_address.substr(descriptor_start));
+  
+  initialize_SocketAddress(ip, port, descriptor);
 }
 
-void SocketAddress::initialize_SocketAddress(std::string ip,
-					     std::string port) {
-  this->is_descriptor = false;
+void SocketAddress::initialize_SocketAddress(std::string ip, std::string port, int descriptor) {
+  if(ip == NO_IP_PORT && port == NO_IP_PORT && descriptor == NO_DESCRIPTOR) {
+    std::cerr << "Error: SocketAddress must at least have valid ip, port or descriptor\n";
+    exit(1);
+  }
   set_ip(ip);
   set_port(port);
-}
-
-void SocketAddress::initialize_SocketAddress(int descriptor) {
-  this->is_descriptor = true;
   set_descriptor(descriptor);
 }
 
-
 std::string SocketAddress::get_ip() const {
-  if(get_is_descriptor()) {
-    access_error("ip", "descriptor");
+  if(!has_ip_port()) {
+    access_error("ip");
   }
   
-  return ip_and_port.ip;
+  return ip;
 }
 
 std::string SocketAddress::get_port() const {
-  if(get_is_descriptor()) {
-    access_error("port", "descriptor");
+  if(!has_ip_port()) {
+    access_error("port");
   }
   
-  return ip_and_port.port;
+  return port;
 }
 
 int SocketAddress::get_descriptor() const {
-  if(!get_is_descriptor()) {
-    access_error("descriptor", "ip_and_port");
+  if(!has_descriptor()) {
+    access_error("descriptor");
   }
 
   return descriptor;
 }
 
-bool SocketAddress::get_is_descriptor() const {
-  return is_descriptor;
+bool SocketAddress::has_descriptor() const {
+  return descriptor != NO_DESCRIPTOR;
+}
+
+bool SocketAddress::has_ip_port() const {
+  return ip.compare(NO_IP_PORT) != 0 || port.compare(NO_IP_PORT) != 0;
 }
 
 void SocketAddress::set_ip(std::string ip) {
-  if(get_is_descriptor()) {
-    access_error("ip", "descriptor");
-  }
-  this->ip_and_port.ip = ip;
+  this->ip = ip;
 }
 
 void SocketAddress::set_port(std::string port) {
-  if(get_is_descriptor()) {
-    access_error("port", "descriptor");
-  }
-  this->ip_and_port.port = port;
+  this->port = port;
 }
 
 void SocketAddress::set_descriptor(int descriptor) {
-  if(!get_is_descriptor()) {
-    access_error("descriptor", "ip_and_port");
-  }
   this->descriptor = descriptor;
 }
 
-void SocketAddress::access_error(const char* accessing, const char* socket_address_type) const {
-  std::cout << "Error: accessing " << accessing << " in SocketAddress of type " << socket_address_type << std::endl;
+void SocketAddress::access_error(const std::string accessing) const {
+  std::cerr << "Error: accessing " << accessing << " but this SocketAddress does not have " << accessing << std::endl;
   exit(1);
 }
 
 bool SocketAddress::operator<(const SocketAddress &other) const {
-  if(get_is_descriptor() != other.get_is_descriptor()) {
-    return get_is_descriptor();
+  int comparison = 0;
+
+  comparison = compare_indicators(other);
+
+  if(comparison == 0) {
+    comparison = compare_ip_port(other);
   }
 
-  int compare_val;
-  
-  if(get_is_descriptor()) {
-    compare_val = get_descriptor() - other.get_descriptor();
+  if(comparison == 0) {
+    comparison = compare_descriptor(other);
+  }
 
-  } else {
-    compare_val = get_ip().compare(other.get_ip());
-    if(compare_val == 0) {
-      compare_val = get_port().compare(other.get_port());
+  return comparison < 0;
+}
+
+int SocketAddress::compare_indicators(const SocketAddress &other) const {
+  int comparison = 0;
+  if(has_ip_port() != other.has_ip_port()) {
+    comparison = has_ip_port()? -1: 1;
+
+  } else if(has_descriptor() != other.has_descriptor()) {
+    comparison = has_descriptor()? -1: 1;
+  }
+
+  return comparison;
+}
+
+//assumed that has_ip_port() == other.has_ip_port()
+int SocketAddress::compare_ip_port(const SocketAddress &other) const {
+  int comparison = 0;
+  if(has_ip_port()) {
+    comparison = get_ip().compare(other.get_ip());
+
+    if(comparison == 0) {
+      comparison = get_port().compare(other.get_port());
     }
   }
-  
 
-  return compare_val < 0;
+  return comparison;
+}
+
+//assumed that has_descriptor() == other.has_descriptor()
+int SocketAddress::compare_descriptor(const SocketAddress &other) const {
+  int comparison = 0;
+  if(has_descriptor()) {
+    comparison = get_descriptor() - other.get_descriptor();
+  }
+
+  return comparison;
 }
 
 std::string SocketAddress::as_string() const {
-  std::string represent_address;
-  if(get_is_descriptor()) {
-    represent_address = DESCRIPTOR_HEADER + std::to_string(get_descriptor());
+  std::string ip_str;
+  std::string port_str;
+  std::string descriptor_str;
 
+  if(has_ip_port()) {
+    ip_str = get_ip();
+    port_str = get_port();
   } else {
-    represent_address = get_ip() + IP_PORT_SEP + get_port();
+    ip_str = NO_IP_PORT;
+    port_str = NO_IP_PORT;
   }
+
+  if(has_descriptor()) {
+    descriptor_str = std::to_string(get_descriptor());
+  } else {
+    descriptor_str = std::to_string(NO_DESCRIPTOR);
+  }
+  
+  std::string represent_address = ip_str + FIELD_SEP + port_str + FIELD_SEP + descriptor_str;
   
   return represent_address;
 }
@@ -142,13 +179,13 @@ bool check_equality(SocketAddress addr1, SocketAddress addr2) {
 }
 
 int main(int argc, char* argv[]) {
-  SocketAddress empty_port("hello", "");
-  SocketAddress empty_ip("", "goodbye");
-  SocketAddress empty_empty("", "");
+  SocketAddress empty_port("hello", NO_IP_PORT);
+  SocketAddress empty_ip(NO_IP_PORT, "goodbye");
+  SocketAddress empty_empty(NO_IP_PORT, NO_IP_PORT, 2);
   SocketAddress basic("hello", "goodbye");
-  SocketAddress descriptor1(1);
+  SocketAddress descriptor1("hello", "goodbye", 1);
   SocketAddress descriptor0(0);
-  SocketAddress descriptorn1(-1);
+  SocketAddress descriptorn1(-2);
   
   std::string empty_port_str = empty_port.as_string();
   std::string empty_ip_str = empty_ip.as_string();
@@ -162,38 +199,37 @@ int main(int argc, char* argv[]) {
   SocketAddress empty_ip_2 = SocketAddress(empty_ip_str);
   SocketAddress empty_empty_2 = SocketAddress(empty_empty_str);
   SocketAddress basic_2 = SocketAddress(basic_str);
-
   SocketAddress descriptor1_2 = SocketAddress(descriptor1_str);
   SocketAddress descriptor0_2 = SocketAddress(descriptor0_str);
   SocketAddress descriptorn1_2 = SocketAddress(descriptorn1_str);
   
   std::string conversion_error = "failed conversion to/from string";
   if(!check_equality(empty_port, empty_port_2)) {
-    std::cout << "empty_port " << conversion_error << "\n";
+    std::cerr << "empty_port " << conversion_error << "\n";
     exit(1);
   }
   if(!check_equality(empty_ip, empty_ip_2)) {
-    std::cout << "empty_ip " << conversion_error << "\n";
+    std::cerr << "empty_ip " << conversion_error << "\n";
     exit(1);
   }
   if(!check_equality(empty_empty, empty_empty_2)) {
-    std::cout << "empty_empty " << conversion_error << "\n";
+    std::cerr << "empty_empty " << conversion_error << "\n";
     exit(1);
   }
   if(!check_equality(basic, basic_2)) {
-    std::cout << "basic " << conversion_error << "\n";
+    std::cerr << "basic " << conversion_error << "\n";
     exit(1);
   }
   if(!check_equality(descriptor1, descriptor1_2)) {
-    std::cout << "basic " << conversion_error << "\n";
+    std::cerr << "basic " << conversion_error << "\n";
     exit(1);
   }
   if(!check_equality(descriptor0, descriptor0_2)) {
-    std::cout << "basic " << conversion_error << "\n";
+    std::cerr << "basic " << conversion_error << "\n";
     exit(1);
   }
   if(!check_equality(descriptorn1, descriptorn1_2)) {
-    std::cout << "basic " << conversion_error << "\n";
+    std::cerr << "basic " << conversion_error << "\n";
     exit(1);
   }
     
--- functionality/socket_address.hpp
+++ functionality/socket_address.hpp
@@ -9,47 +9,48 @@
 #include <iostream>
 #include <iterator>
 
-#define IP_PORT_SEP ('$')
-#define DESCRIPTOR_HEADER ('#')
+#define FIELD_SEP ('$')
+#define NO_IP_PORT ("")
+#define NO_DESCRIPTOR (-1)
 
-
-struct IPPort;
 class SocketAddress;
-
-struct IPPort {
-  std::string ip;
-  std::string port;
-};
 
 class SocketAddress: public Stringable, public Address {
 private:
+  bool has_ip_port_indicator;
+  bool has_descriptor_indicator;
 
-  bool is_descriptor;
-  
-  struct IPPort ip_and_port;
-  int descriptor;
+  std::string ip;
+  std::string port;
+  int descriptor = -1;
 
   void get_error(const std::string getting, const std::string socket_address_type);
   
 public:
   SocketAddress(std::string ip, std::string port);
   SocketAddress(int descriptor);
+  SocketAddress(std::string ip, std::string port, int descriptor);
   SocketAddress(std::string represent_address);
   
   void initialize_SocketAddress(std::string ip,
-				std::string port);
-  void initialize_SocketAddress(int descriptor);
+				std::string port,
+				int descriptor);
 
   std::string get_ip() const;
   std::string get_port() const;
   int get_descriptor() const;
-  bool get_is_descriptor() const;
+  bool has_ip_port() const;
+  bool has_descriptor() const;
   void set_ip(std::string ip);
   void set_port(std::string port);
   void set_descriptor(int descriptor);
-  void access_error(const char* accessing, const char* socket_address_type) const;
-  bool operator<(const SocketAddress &other) const;
+  void access_error(const std::string accessing) const;
 
+  bool operator<(const SocketAddress &other) const;
+  int compare_indicators(const SocketAddress &other) const;
+  int compare_ip_port(const SocketAddress &other) const;
+  int compare_descriptor(const SocketAddress &other) const;
+  
   virtual std::string as_string() const override;
 };
 
