fe5e00570578aaa4fe140f1b96f7fca3daa7d797
Diego Esparza <dae1@williams.edu>
2021-08-25 20:59:22 -0400

    abstract, used in communicators

--- functionality/address.hpp
+++ functionality/address.hpp
@@ -1,35 +1,8 @@
 #ifndef ADDRESS_HPP
 #define ADDRESS_HPP
 
+class Address {
 
-#include "stringable.hpp"
-
-#include <string>
-#include <iostream>
-#include <iterator>
-
-#define IP_PORT_SEP ('$')
-
-class Address: public Stringable {
-private:
-  std::basic_string<char> ip;
-  std::basic_string<char> port;
-  
-public:
-  Address(std::basic_string<char> ip, std::basic_string<char> port);
-  Address(std::basic_string<char> represent_address);
-  void initialize_Address(std::basic_string<char> ip,
-			  std::basic_string<char> port);
-
-
-  std::basic_string<char> get_ip() const;
-  std::basic_string<char> get_port() const;
-  void set_ip(std::basic_string<char> ip);
-  void set_port(std::basic_string<char> port);
-
-  bool operator<(const Address &other) const;
-
-  virtual std::basic_string<char> as_string() const override;
 };
 
 
