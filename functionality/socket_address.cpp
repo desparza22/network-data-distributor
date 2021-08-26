183e30e439aad5c5449a96b23938682d28be35d8
Diego Esparza <dae1@williams.edu>
2021-08-25 20:58:44 -0400

    inherits from Address, can hold descriptor instead of ip/port

--- functionality/socket_address.cpp
+++ functionality/socket_address.cpp
@@ -4,61 +4,136 @@ SocketAddress::SocketAddress(std::string ip, std::string port) {
   initialize_SocketAddress(ip, port);
 }
 
-//represent_address[read_index] should come from as_string
-SocketAddress::SocketAddress(std::string represent_address) {
-  std::string ip;
-  std::string port;
-
-  int ip_end = represent_address.find(IP_PORT_SEP);
-  int port_start = ip_end + 1;
-
-  ip = represent_address.substr(0, ip_end);
-  port = represent_address.substr(port_start);
-
-  initialize_SocketAddress(ip, port);
+SocketAddress::SocketAddress(int descriptor) {
+  initialize_SocketAddress(descriptor);
 }
 
+//represent_address should come from as_string
+SocketAddress::SocketAddress(std::string represent_address) {
+  char first_char = represent_address[0];
+  
+  if(first_char == DESCRIPTOR_HEADER) {
+    int descriptor_start = 1;
+    std::string descriptor_str = represent_address.substr(descriptor_start);
+
+    int descriptor = std::stoi(descriptor_str);
+    initialize_SocketAddress(descriptor);
+    
+  } else {
+    std::string ip;
+    std::string port;
+  
+    int ip_end = represent_address.find(IP_PORT_SEP);
+    int port_start = ip_end + 1;
+  
+    ip = represent_address.substr(0, ip_end);
+    port = represent_address.substr(port_start);
+    initialize_SocketAddress(ip, port);
+  }
+}
 
 void SocketAddress::initialize_SocketAddress(std::string ip,
-				 std::string port) {
-  this->ip = ip;
-  this->port = port;
+					     std::string port) {
+  this->is_descriptor = false;
+  set_ip(ip);
+  set_port(port);
+}
+
+void SocketAddress::initialize_SocketAddress(int descriptor) {
+  this->is_descriptor = true;
+  set_descriptor(descriptor);
 }
 
 
 std::string SocketAddress::get_ip() const {
-  return ip;
+  if(get_is_descriptor()) {
+    access_error("ip", "descriptor");
+  }
+  
+  return ip_and_port.ip;
 }
 
 std::string SocketAddress::get_port() const {
-  return port;
+  if(get_is_descriptor()) {
+    access_error("port", "descriptor");
+  }
+  
+  return ip_and_port.port;
+}
+
+int SocketAddress::get_descriptor() const {
+  if(!get_is_descriptor()) {
+    access_error("descriptor", "ip_and_port");
+  }
+
+  return descriptor;
+}
+
+bool SocketAddress::get_is_descriptor() const {
+  return is_descriptor;
 }
 
 void SocketAddress::set_ip(std::string ip) {
-  this->ip = ip;
+  if(get_is_descriptor()) {
+    access_error("ip", "descriptor");
+  }
+  this->ip_and_port.ip = ip;
 }
 
 void SocketAddress::set_port(std::string port) {
-  this->port = port;
+  if(get_is_descriptor()) {
+    access_error("port", "descriptor");
+  }
+  this->ip_and_port.port = port;
+}
+
+void SocketAddress::set_descriptor(int descriptor) {
+  if(!get_is_descriptor()) {
+    access_error("descriptor", "ip_and_port");
+  }
+  this->descriptor = descriptor;
+}
+
+void SocketAddress::access_error(const char* accessing, const char* socket_address_type) const {
+  std::cout << "Error: accessing " << accessing << " in SocketAddress of type " << socket_address_type << std::endl;
+  exit(1);
 }
 
 bool SocketAddress::operator<(const SocketAddress &other) const {
-  int compare_val = ip.compare(other.get_ip());
-  if(compare_val == 0) {
-    compare_val = port.compare(other.get_port());
+  if(get_is_descriptor() != other.get_is_descriptor()) {
+    return get_is_descriptor();
   }
 
-  return compare_val;
+  int compare_val;
+  
+  if(get_is_descriptor()) {
+    compare_val = get_descriptor() - other.get_descriptor();
+
+  } else {
+    compare_val = get_ip().compare(other.get_ip());
+    if(compare_val == 0) {
+      compare_val = get_port().compare(other.get_port());
+    }
+  }
+  
+
+  return compare_val < 0;
 }
 
 std::string SocketAddress::as_string() const {
-  std::string represent_address = ip + IP_PORT_SEP + port;
+  std::string represent_address;
+  if(get_is_descriptor()) {
+    represent_address = DESCRIPTOR_HEADER + std::to_string(get_descriptor());
 
+  } else {
+    represent_address = get_ip() + IP_PORT_SEP + get_port();
+  }
+  
   return represent_address;
 }
 
 
-#ifdef TEST_ADDRESS
+#ifdef TEST_SOCKET_ADDRESS
 
 bool check_equality(SocketAddress addr1, SocketAddress addr2);
 
@@ -71,32 +146,53 @@ int main(int argc, char* argv[]) {
   SocketAddress empty_ip("", "goodbye");
   SocketAddress empty_empty("", "");
   SocketAddress basic("hello", "goodbye");
+  SocketAddress descriptor1(1);
+  SocketAddress descriptor0(0);
+  SocketAddress descriptorn1(-1);
   
   std::string empty_port_str = empty_port.as_string();
   std::string empty_ip_str = empty_ip.as_string();
   std::string empty_empty_str = empty_empty.as_string();
   std::string basic_str = basic.as_string();
+  std::string descriptor1_str = descriptor1.as_string();
+  std::string descriptor0_str = descriptor0.as_string();
+  std::string descriptorn1_str = descriptorn1.as_string();
 
-  
-  SocketAddress empty_port2 = SocketAddress(empty_port_str);
-  SocketAddress empty_ip2 = SocketAddress(empty_ip_str);
-  SocketAddress empty_empty2 = SocketAddress(empty_empty_str);
-  SocketAddress basic2 = SocketAddress(basic_str);
+  SocketAddress empty_port_2 = SocketAddress(empty_port_str);
+  SocketAddress empty_ip_2 = SocketAddress(empty_ip_str);
+  SocketAddress empty_empty_2 = SocketAddress(empty_empty_str);
+  SocketAddress basic_2 = SocketAddress(basic_str);
+
+  SocketAddress descriptor1_2 = SocketAddress(descriptor1_str);
+  SocketAddress descriptor0_2 = SocketAddress(descriptor0_str);
+  SocketAddress descriptorn1_2 = SocketAddress(descriptorn1_str);
   
   std::string conversion_error = "failed conversion to/from string";
-  if(!check_equality(empty_port, empty_port2)) {
+  if(!check_equality(empty_port, empty_port_2)) {
     std::cout << "empty_port " << conversion_error << "\n";
     exit(1);
   }
-  if(!check_equality(empty_ip, empty_ip2)) {
+  if(!check_equality(empty_ip, empty_ip_2)) {
     std::cout << "empty_ip " << conversion_error << "\n";
     exit(1);
   }
-  if(!check_equality(empty_empty, empty_empty2)) {
+  if(!check_equality(empty_empty, empty_empty_2)) {
     std::cout << "empty_empty " << conversion_error << "\n";
     exit(1);
   }
-  if(!check_equality(basic, basic2)) {
+  if(!check_equality(basic, basic_2)) {
+    std::cout << "basic " << conversion_error << "\n";
+    exit(1);
+  }
+  if(!check_equality(descriptor1, descriptor1_2)) {
+    std::cout << "basic " << conversion_error << "\n";
+    exit(1);
+  }
+  if(!check_equality(descriptor0, descriptor0_2)) {
+    std::cout << "basic " << conversion_error << "\n";
+    exit(1);
+  }
+  if(!check_equality(descriptorn1, descriptorn1_2)) {
     std::cout << "basic " << conversion_error << "\n";
     exit(1);
   }
--- functionality/socket_address.hpp
+++ functionality/socket_address.hpp
@@ -3,30 +3,51 @@
 
 
 #include "stringable.hpp"
+#include "address.hpp"
 
 #include <string>
 #include <iostream>
 #include <iterator>
 
 #define IP_PORT_SEP ('$')
+#define DESCRIPTOR_HEADER ('#')
 
-class SocketAddress: public Stringable {
-private:
+
+struct IPPort;
+class SocketAddress;
+
+struct IPPort {
   std::string ip;
   std::string port;
+};
+
+class SocketAddress: public Stringable, public Address {
+private:
+
+  bool is_descriptor;
+  
+  struct IPPort ip_and_port;
+  int descriptor;
+
+  void get_error(const std::string getting, const std::string socket_address_type);
   
 public:
   SocketAddress(std::string ip, std::string port);
+  SocketAddress(int descriptor);
   SocketAddress(std::string represent_address);
+  
   void initialize_SocketAddress(std::string ip,
 				std::string port);
-
+  void initialize_SocketAddress(int descriptor);
 
   std::string get_ip() const;
   std::string get_port() const;
+  int get_descriptor() const;
+  bool get_is_descriptor() const;
   void set_ip(std::string ip);
   void set_port(std::string port);
-
+  void set_descriptor(int descriptor);
+  void access_error(const char* accessing, const char* socket_address_type) const;
   bool operator<(const SocketAddress &other) const;
 
   virtual std::string as_string() const override;
